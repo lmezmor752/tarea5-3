c0b6551e361cd09c643a1bd1fe46d2c99e6b5296
lmezmor752 <132547384+lmezmor752@users.noreply.github.com>
2024-05-20 10:08:15 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    }
+    if (receivedNumber == 2) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    }
+    if (receivedNumber == 3) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    }
+    if (receivedNumber == 4) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+    }
+})
+let strip: neopixel.Strip = null
+radio.setGroup(95)
+strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
